7a86ae9c7ef60d2988623c4e469420322d979cde
Bogdan <bohdan.husak@gmail.com>
2019-06-04 10:50:44 +0300

    Added initial api

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jcf_api.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcf_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHostingEnvironment env;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            this.env = env ?? throw new ArgumentNullException(nameof(env));
+        }
+
+        [HttpGet("~/")]
+        public IActionResult Ping()
+        {
+            return Ok(new 
+            {
+                name = "jcf-api",
+                environment = env.EnvironmentName,
+                time_zone = "UTC",
+                start_time = StartupHelper.StartupTime.ToString("d MMM yyyy HH:mm:ss")
+            });
+        }
+    }
+}
---
+++ Helpers/StartupHelper.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace jcf_api.Helpers
+{
+    public class StartupHelper
+    {
+        public static DateTime StartupTime { get; set; }
+    }
+}
